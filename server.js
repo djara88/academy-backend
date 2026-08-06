b718ca63fc5f6ef83ef63ed55c66bc6df056269f
djara88 <d.jarazerene@gmail.com>
2026-08-06 14:41:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const authMiddleware = require('./middleware/auth');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// 🔥 LÍMITES AUMENTADOS A 50MB PARA PERMITIR PDFs PESADOS
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get('/', (req, res) => {
   res.send('API de Academia Multi-tenant funcionando 🚀');
